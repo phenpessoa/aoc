5c3214866c76073ec1c19162c08e600a39199bb5
phenpessoa <pedro_santos_40@hotmail.com>
2023-02-03 18:21:56 -0300

    js: 2022/001: add solution

---
+++ js/2022/001/001.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function doMagic(file) {
+    const fileStream = fs.createReadStream(file);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let vals = [];
+    let cur = 0;
+
+    for await (const txt of rl) {
+        if (txt.length == 0) {
+            vals.push(cur)
+            cur = 0
+            continue
+        }
+        cur += +txt;
+    }
+
+    if (cur !== 0) {
+        vals.push(cur)
+    }
+
+    vals.sort((a, b) => {
+        return a-b;
+    });
+
+    console.log(vals[vals.length-1]);
+    console.log(vals[vals.length-1]+vals[vals.length-2]+vals[vals.length-3]);
+}
+
+doMagic('example.txt');
+doMagic('input.txt');
