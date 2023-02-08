811441ddb971f1cd191a7a3460dbda6981cd93d2
phenpessoa <pedro_santos_40@hotmail.com>
2023-02-07 21:53:33 -0300

    rust: run cargo fmt

--- rust/src/aoc_2022/day_001/mod.rs
+++ rust/src/aoc_2022/day_001/mod.rs
@@ -3,24 +3,20 @@ use std::error::Error;
 const EXAMPLE_INPUT: &str = include_str!("example.txt");
 const INPUT: &str = include_str!("input.txt");
 
-pub fn do_magic(from: &str) -> Result<[isize; 2],Box<dyn Error>> {
+pub fn do_magic(from: &str) -> Result<[isize; 2], Box<dyn Error>> {
     let mut vals: Vec<isize> = Vec::new();
     let mut cur: isize = 0;
     let data: &str = match from {
         "example" => EXAMPLE_INPUT,
         "input" => INPUT,
-        _ => {
-            return Err("unknown input".into())
-        }
+        _ => return Err("unknown input".into()),
     };
 
-    let lines: std::str::Lines  = data.lines();
+    let lines: std::str::Lines = data.lines();
 
     for line in lines {
         match line.parse::<isize>() {
-            Ok(val) => {
-                cur += val
-            }
+            Ok(val) => cur += val,
             Err(_) => {
                 vals.push(cur);
                 cur = 0;
@@ -36,7 +32,7 @@ pub fn do_magic(from: &str) -> Result<[isize; 2],Box<dyn Error>> {
     vals.sort();
 
     Ok([
-        vals[vals.len()-1],
-        vals[vals.len()-1]+vals[vals.len()-2]+vals[vals.len()-3],
+        vals[vals.len() - 1],
+        vals[vals.len() - 1] + vals[vals.len() - 2] + vals[vals.len() - 3],
     ])
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,15 +10,14 @@ fn main() {
     }
 
     let year: usize = args[1]
-    .trim()
-    .parse()
-    .expect("year must be a positive number");
+        .trim()
+        .parse()
+        .expect("year must be a positive number");
 
     let day: usize = args[2]
-    .trim()
-    .parse()
-    .expect("day must be a positive number");
-
+        .trim()
+        .parse()
+        .expect("day must be a positive number");
 
     match year {
         2022 => match day {
@@ -28,7 +27,7 @@ fn main() {
                     aoc_2022::day_001::do_magic("example"),
                     aoc_2022::day_001::do_magic("input")
                 )
-            },
+            }
             _ => print!("day {} of year {} has no solutions", day, year),
         },
         _ => print!("year {} has no solutions", year),
